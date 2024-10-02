479ac1df71894cd16659661249ae82a27d3fd6f6
mlbyvn <diogen.de@yahoo.com>
2024-10-02 20:13:54 +0200

    Add files via upload

---
+++ script/Interactions.s.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+
+/**
+ * @dev Script funds the depolyed on testnet (sepolia or anvil) smart
+ * contract in order to run some tests
+ */
+
+contract FundFundMe is Script {
+    uint256 SEND_VALUE = 0.1 ether;
+
+    function fundFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        vm.stopBroadcast();
+        console.log("Funded FundMe with %s", SEND_VALUE);
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        fundFundMe(mostRecentlyDeployed);
+    }
+}
+
+/**
+ * @dev Script withdraws money from most recently deployed (on a testnet) FundMe contract.
+ * Needed to run some test efficiently.
+ */
+
+contract WithdrawFundMe is Script {
+    function withdrawFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).withdraw();
+        vm.stopBroadcast();
+        console.log("Withdraw FundMe balance!");
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        withdrawFundMe(mostRecentlyDeployed);
+    }
+}
